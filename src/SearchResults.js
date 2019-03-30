ffb31fc870e2ebbbf347b48d4fe539a1330a62ae
Juan Pablo Volpintesta <jvolpintesta@gmail.com>
2019-03-30 21:33:59 +0000

    No results found
    Adding no results found feature

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./SearchResults.css";
 
 const SearchResults = props => {
-  return (
+  return props.afterReceivingResults.length > 0 ? (
     <ul className="jokes-list">
       {props.afterReceivingResults.map(item => (
         <li key={item.id}>{item.joke}</li>
       ))}
     </ul>
+  ) : (
+    "no results found!"
   );
 };
 
